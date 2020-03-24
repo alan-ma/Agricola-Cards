c77db798362f6e15aae612331f0571a8633f9f4a
Alan Ma <alan_ma@live.ca>
2020-03-24 02:11:00 -0400

    images and script

---
+++ README.md
@@ -0,0 +1,13 @@
+# Agricola-Cards
+
+Script to generate easily printable cards for the Agricola game.
+
+Images from the following sources used in the [online game](http://playagricola.com/Agricola/Board1/Agricola.html):
+
+http://playagricola.com/Agricola/Board20/allcardse.html
+http://playagricola.com/Agricola/Board20/allcardsg2.php?s=chris-G3yes
+http://playagricola.com/Agricola/Board20/allcardsg.php
+http://playagricola.com/Agricola/Board20/allcardsi.html
+http://playagricola.com/Agricola/Board20/allcardsk.html
+http://playagricola.com/Agricola/Board20/allcardsg2.php?s=chris-G2
+
---
+++ process.py
@@ -0,0 +1,64 @@
+from fpdf import FPDF
+import os
+
+
+# measurements in mm
+CARD_HEIGHT = 96
+SPACING = 2
+HORIZONTAL_SPACING = 65
+MARGIN = 10
+
+
+class PDF(FPDF):
+    # Page footer
+    def footer(self):
+        # Position at 1.5cm from bottom
+        self.set_y(-10)
+        # Courier 8
+        self.set_font('Courier', '', 8)
+        # Page number
+        text = self.title + ' - Page ' + str(self.page_no()) + '/{nb}'
+        self.cell(0, 10, text, 0, 0, 'C')
+
+
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
+
+def main():
+    for deck in os.listdir('images'):
+        # Landscape, pt units, Letter
+        pdf = PDF('L', 'mm', 'Letter')
+        pdf.alias_nb_pages()
+        pdf.set_title(deck)
+        pdf.set_font('Courier', '', 10)
+
+        images = os.listdir('images/%s' % deck)
+        images = [image for image in images if image[-4:] == '.jpg']
+        for chunk in chunks(images, 8):
+            pdf.add_page()
+            for i, image in enumerate(chunk[:4]):
+                path = 'images/%s/%s' % (deck, image)
+                pdf.image(
+                    path,
+                    x=HORIZONTAL_SPACING * i + MARGIN,
+                    y=MARGIN,
+                    h=CARD_HEIGHT,
+                )
+            for i, image in enumerate(chunk[4:]):
+                path = 'images/%s/%s' % (deck, image)
+                pdf.image(
+                    path,
+                    x=HORIZONTAL_SPACING * i + MARGIN,
+                    y=CARD_HEIGHT + SPACING + MARGIN,
+                    h=CARD_HEIGHT,
+                )
+
+        # output to file
+        pdf.output('output/%s.pdf' % deck, 'F')
+
+
+if __name__ == '__main__':
+    main()
